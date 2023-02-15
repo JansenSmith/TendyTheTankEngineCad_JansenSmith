88a2cd7432dace52f2a46349b2e739adf8189e30
JansenSmith <jansen@bstarengineering.com>
2023-02-15 17:04:19 -0500

    copy file content

---
+++ FromSunlightWalk.groovy
@@ -0,0 +1,14 @@
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
+
+// code here
+
+return new com.neuronrobotics.sdk.addons.kinematics.IDriveEngine (){
+
+	@Override
+	public void DriveArc(MobileBase arg0, TransformNR arg1, double arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
