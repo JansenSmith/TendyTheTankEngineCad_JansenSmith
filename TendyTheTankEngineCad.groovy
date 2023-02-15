b1066528ec7a9ea3f357b4b30e2ce867e0140281
JansenSmith <jansen@bstarengineering.com>
2023-02-15 17:21:28 -0500

    copy file content

---
+++ TendyTheTankEngineCad.groovy
@@ -0,0 +1,39 @@
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+
+// code here
+
+return new ICadGenerator(){
+
+	@Override
+	public ArrayList<CSG> generateCad(DHParameterKinematics arg0, int arg1) {
+		// TODO Auto-generated method stub
+		ArrayList<CSG> back =[]
+		back.add(new Cube(1).toCSG())
+		for(CSG c:back)
+			c.setManipulator(arg0.getLinkObjectManipulator(arg1))
+		return back;
+	}
+
+	@Override
+	public ArrayList<CSG> generateBody(MobileBase arg0) {
+		// TODO Auto-generated method stub
+		ArrayList<CSG> back =[]
+		back.add(new Cube(1).toCSG())
+		for(CSG c:back)
+			c.setManipulator(arg0.getRootListener())
+		for(DHParameterKinematics kin:arg0.getAllDHChains()) {
+			CSG limbRoot =new Cube(1).toCSG()
+			limbRoot.setManipulator(kin.getRootListener())
+			back.add(limbRoot)
+
+		}
+		return back;
+	}
+	
+	
+}
