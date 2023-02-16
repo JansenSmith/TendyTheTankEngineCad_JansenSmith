5d48184a3fc9b456e8d0f2e710f58c011fe467f7
JansenSmith <jansen@bstarengineering.com>
2023-02-15 19:57:45 -0500

    added parameters

--- TendyTheTankEngineCad.groovy
+++ TendyTheTankEngineCad.groovy
@@ -4,6 +4,8 @@ import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Sphere
+import eu.mihosoft.vrl.v3d.parametrics.LengthParameter
 
 // code here
 
@@ -23,7 +25,14 @@ return new ICadGenerator(){
 	public ArrayList<CSG> generateBody(MobileBase arg0) {
 		// TODO Auto-generated method stub
 		ArrayList<CSG> back =[]
-		back.add(new Cube(1).toCSG())
+		back.add(new Sphere(1).toCSG())
+		
+		boardThickness = new LengthParameter("Board Thickness (mm)", 3, [0, 100])
+		bayDepth = new LengthParameter("Bay Depth (mm)", 400, [0, 1000])
+		bayWidth = new LengthParameter("Bay Width (mm)", 400, [0, 1000])
+		bayHeight = new LengthParameter("Bay Height (mm)", 1000, [0, 5000])
+		
+		
 		for(CSG c:back)
 			c.setManipulator(arg0.getRootListener())
 		for(DHParameterKinematics kin:arg0.getAllDHChains()) {
