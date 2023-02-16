76a8c5ee8d7b1a9c9c5ee844fb5dff9de36b101b
JansenSmith <jansen@bstarengineering.com>
2023-02-16 15:09:00 -0500

    got a plate with a hole

--- TendyTheTankEngineCad.groovy
+++ TendyTheTankEngineCad.groovy
@@ -4,6 +4,7 @@ import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.Sphere
 import eu.mihosoft.vrl.v3d.parametrics.LengthParameter
 
@@ -27,10 +28,25 @@ return new ICadGenerator(){
 		ArrayList<CSG> back =[]
 		back.add(new Sphere(1).toCSG())
 		
-		boardThickness = new LengthParameter("Board Thickness (mm)", 3, [0, 100])
-		bayDepth = new LengthParameter("Bay Depth (mm)", 400, [0, 1000])
-		bayWidth = new LengthParameter("Bay Width (mm)", 400, [0, 1000])
-		bayHeight = new LengthParameter("Bay Height (mm)", 1000, [0, 5000])
+		LengthParameter bucketDiameter = new LengthParameter("Bucket Diameter (mm)", 304.8, [0, 1000])
+		LengthParameter boardThickness = new LengthParameter('Board Thickness (mm)', 3, [0, 100])
+		LengthParameter bayDepth = new LengthParameter("Bay Depth (mm)", 400, [0, 1000])
+		LengthParameter bayWidth = new LengthParameter("Bay Width (mm)", 400, [0, 1000])
+		LengthParameter bayHeight = new LengthParameter("Bay Height (mm)", 1000, [0, 5000])
+		
+		CSG plantShelf = new Cube(bayDepth.getMM()/2, bayWidth.getMM(), boardThickness.getMM()).toCSG()
+			.movex(bayDepth.getMM()/4)
+			.movez(boardThickness.getMM()/2)
+			.rotz(90)
+		CSG bucketGhost = new Cylinder(bucketDiameter.getMM()/2,boardThickness.getMM(), (int) 100).toCSG()
+		plantShelf = plantShelf.difference(bucketGhost)
+		
+		
+		
+		
+		
+		
+		back.add(plantShelf)
 		
 		
 		for(CSG c:back)
