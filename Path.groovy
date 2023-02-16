536a9629f9ab9ad88500a0ead5c1bca9c5ab6f3a
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-16 11:23:51 -0500

    Adding bezier

--- Path.groovy
+++ Path.groovy
@@ -1,1 +1,11 @@
 // code here
+def URL="https://github.com/TechnocopiaPlant/TendyTheTankEngine.git"
+
+def numBezierPieces = 15
+BezierEditor editor = new BezierEditor(ScriptingEngine.fileFromGit(URL, "bez.json"),10)
+BezierEditor editor2 = new BezierEditor(ScriptingEngine.fileFromGit(URL, "bez2.json"),numBezierPieces)
+BezierEditor editor3 = new BezierEditor(ScriptingEngine.fileFromGit(URL, "bez3.json"),numBezierPieces)
+
+
+editor.addBezierToTheEnd(editor2)
+editor2.addBezierToTheEnd(editor3)
