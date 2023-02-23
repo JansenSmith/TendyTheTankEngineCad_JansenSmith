33c97ad797975cb135e39619fe5d1166709c2000
JansenSmith <jansen@bstarengineering.com>
2023-02-22 22:44:53 -0500

    almost finished tab adder

--- TendyTheTankEngineCad.groovy
+++ TendyTheTankEngineCad.groovy
@@ -35,13 +35,17 @@ return new ICadGenerator(){
 		LengthParameter boardThickness = new LengthParameter('Board Thickness (mm)', 19, [0, 100])			// 4'x8' plywood sheets
 		boardThickness.setMM(19)
 		LengthParameter bayDepth = new LengthParameter("Bay Depth (mm)", 400, [0, 1000])
-		bayDepth.setMM(400)
+		bayDepth.setMM(379.5)
+		//bayDepth.setMM(390)
+		//bayDepth.setMM(400)
+		//bayDepth.setMM(420)
+		//bayDepth.setMM(440)
 		LengthParameter bayWidth = new LengthParameter("Bay Width (mm)", 400, [0, 1000])
 		bayWidth.setMM(400)
 		LengthParameter bayHeight = new LengthParameter("Bay Height (mm)", 1000, [0, 5000])
 		bayHeight.setMM(1000)
 		LengthParameter railElevation = new LengthParameter("Rail Elevation (mm)", 200, [0, 1000])
-		railElevation.setMM(200)
+		railElevation.setMM(400)
 		LengthParameter trackDistFromWall = new LengthParameter("Track Distance from Wall (mm)", 75, [0, 1000])
 		trackDistFromWall.setMM(75)
 		
@@ -72,14 +76,45 @@ return new ICadGenerator(){
 		plantShelf = plantShelf.union(armShelfPort)
 		plantShelf = plantShelf.union(armShelfStarboard)
 		
-		
+		CSG boardTemp = plantShelf.rotz(90)
+		def tabTrans = new Transform().movex(-boardTemp.getMinX()).movey(-boardTemp.getMinY()).movez(-boardTemp.getMinZ())
+		boardTemp = boardTemp.transformed(tabTrans)
+		def tabSize = boardTemp.getMaxZ()*2
+		def cycleSize = tabSize*3
+		def minBuffer = tabSize
+		CSG tabTemp = new Cube(tabSize,boardTemp.getMaxZ(),boardTemp.getMaxZ()).toCSG()
+		tabTemp = tabTemp.movex(tabTemp.getMaxX())
+			.movey(-tabTemp.getMaxY()+boardTemp.getMinY())
+			.movez(tabTemp.getMaxZ())
+		def iterNum = (boardTemp.getMaxX() - tabSize - minBuffer*2)/cycleSize // should be the number of full tab-space cycles (so not including the first tab)
+		println(iterNum+1)
+		iterNum = Math.round(iterNum)
+		def bufferVal = (boardTemp.getMaxX() - (tabSize + cycleSize * iterNum)) / 2 // should be the clearance beyond the outermost tabs, equal on both sides. never more than minBuffer.
+		println(boardTemp.getMaxX())
+		println(tabSize+cycleSize*iterNum)
+		println(minBuffer)
+		println(bufferVal)
+		for(double i=0; i<=iterNum; i++) {
+			xVal = bufferVal + i * cycleSize
+			//println(boardTemp.getMaxX())
+			//println(tabSize + cycleSize * iterNum)
+			//println(boardTemp.getMaxX() - (tabSize + cycleSize * iterNum))
+			println(xVal)
+			boardTemp = boardTemp.union(tabTemp.movex(xVal))
+			//println(boardTemp.getMaxX())
+		}
+		plantShelf = boardTemp.transformed(tabTrans.inverse())
+		plantShelf = boardTemp.rotz(-90)
+		back.add(boardTemp.setColor(javafx.scene.paint.Color.MAGENTA))
+		//back.add(tabTemp.setColor(javafx.scene.paint.Color.BLUE))
 		
 		CSG portWall = new Cube(boardThickness.getMM(),bayDepth.getMM(),bayHeight.getMM()/2).toCSG()
 			.movex(bayWidth.getMM()/2 + boardThickness.getMM()/2)
 			.movez(bayHeight.getMM()/4)
+		portWall = portWall.difference(plantShelf)
 		CSG starboardWall = portWall.mirrorx()
 		
-		CSG backWall = new Cube(bayWidth.getMM(), boardThickness.getMM(), bayHeight.getMM()/2).toCSG()
+		CSG backWall = new Cube(bayWidth.getMM()+boardThickness.getMM()*2, boardThickness.getMM(), bayHeight.getMM()/2).toCSG()
 			.movex(0)
 			.movey(-bayDepth.getMM()/2-boardThickness.getMM()/2)
 			.movez(bayHeight.getMM()/4)
@@ -110,11 +145,12 @@ return new ICadGenerator(){
 		
 		CSG trackShelf = portTrack.union(starboardTrack).union(backTrack).union(portTrackArc).union(starboardTrackArc)
 		
-		back.add(plantShelf)
-		back.add(portWall)
-		back.add(starboardWall)
-		back.add(backWall)
-		back.add(trackShelf)
+
+		//back.add(plantShelf.setColor(javafx.scene.paint.Color.MAGENTA))
+		//back.add(portWall)
+		//back.add(starboardWall)
+		//back.add(backWall)
+		//back.add(trackShelf)
 		
 		
 		for(CSG c:back)
@@ -129,6 +165,8 @@ return new ICadGenerator(){
 		
 		return back;
 	}
+	
+	//public CSG addSlots
 	
 	
 }
